47c13387d42360c6f3058e1a2d2471f225530354
Alvin Neri <aneri@dev.empowerwealth.com.au>
2021-09-19 22:14:21 +0800

    try websocket api initial implementation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { useEffect } from "react";
+const socket = new WebSocket("wss://apexapi.bitazza.com/WSGateway");
 const App = () => {
+  useEffect(() => {
+    socket.addEventListener("open", function (event) {
+      console.log("Hello Server");
 
-  return(
-    <div>Bitazza</div>
-  )
-}
+      let param = {
+        Password: "Binbai13!",
+        UserName: "alvin.neri.ece@gmail.com",
+      };
+
+      let frame = {
+        m: 0,
+        i: 2,
+        n: "AuthenticateUser",
+        o: "",
+      };
+
+      frame.o = JSON.stringify(param);
+      socket.send(JSON.stringify(frame));
+    });
+
+    socket.onmessage = (message) => {
+      const _message = JSON.parse(message.data);
+      if (_message.m === 1) {
+        if (_message.n === "AuthenticateUser") {
+          const response = JSON.parse(_message.o);
+          console.log(response);
+        }
+      }
+    };
+  }, []);
+
+  return <div>Bitazza</div>;
+};
 
 export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+export const API_CONSTANTS = {
+  BASE_URL: "wss://apexapi.bitazza.com/WSGateway",
+  AUTH_USER: "AuthenticateUser",
+  LOGOUT: "LogOut",
+};
